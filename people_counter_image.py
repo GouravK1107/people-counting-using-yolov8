6dec5613ea2dd305862248e982f2bdeceff36a2d
GouravK1107 <gkprof1107@gmail.com>
2026-02-26 13:20:35 +0530

    People Counting Project using YOLOv8n

---
+++ people_counter_image.py
@@ -0,0 +1,28 @@
+from ultralytics import YOLO
+import cv2 as cv
+
+model = YOLO("yolov8n.pt") # loading the model 
+
+image_path = "/Python_programming/opencv/CountingPeople/test4.png"
+image = cv.imread(image_path)
+
+if image is None:
+    print("Error: Image is not found !!")
+    exit()
+
+results = model(image)
+person = 0
+
+for i in results:
+    for j in i.boxes:
+        cls_id = int(j.cls[0])
+
+        if cls_id == 0:
+            person += 1
+
+cv.putText(image, f"Counts of peoples: {person}", (50, 20), cv.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
+cv.imshow("People Count terminal !", image)
+cv.waitKey(0)
+cv.destroyAllWindows()
+
+        
