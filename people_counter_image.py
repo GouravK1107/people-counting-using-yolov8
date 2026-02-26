77d94cdd3a021072bf5a9f71a1b78583d477357f
GouravK1107 <gkprof1107@gmail.com>
2026-02-26 13:45:57 +0530

    Updated the people_counter_image.py

--- people_counter_image.py
+++ people_counter_image.py
@@ -16,13 +16,12 @@ person = 0
 for i in results:
     for j in i.boxes:
         cls_id = int(j.cls[0])
+        confident = float(j.conf[0])
 
-        if cls_id == 0:
+        if cls_id == 0 and confident >= 0.5:
             person += 1
 
 cv.putText(image, f"Counts of peoples: {person}", (50, 20), cv.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
 cv.imshow("People Count terminal !", image)
 cv.waitKey(0)
 cv.destroyAllWindows()
-
-        
